3f9c2cf36be6f86e1a1f47395e35be0a01649eb8
Equinoxalt <m.imam26habibie@gmail.com>
2024-12-05 09:17:48 +0700

    Bottom NavBar

---
+++ InstaHELP/app/src/main/java/com/dicoding/instahelp/resident/ResidentMainActivity.kt
@@ -0,0 +1,46 @@
+package com.dicoding.instahelp.resident
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.instahelp.R
+import androidx.fragment.app.Fragment
+import com.dicoding.instahelp.resident.fragment.PanggilFragment
+
+import com.dicoding.instahelp.resident.fragment.PengaturanFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class ResidentMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resident_main)
+
+        // Default fragment
+        loadFragment(PanggilFragment())
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.panggilFragment -> {
+                    loadFragment(PanggilFragment())
+                    true
+                }
+
+                R.id.pengaturanFragment -> {
+                    loadFragment(PengaturanFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
---
+++ InstaHELP/app/src/main/java/com/dicoding/instahelp/resident/fragment/PengaturanFragment.kt
@@ -0,0 +1,44 @@
+package com.dicoding.instahelp.resident.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import com.dicoding.instahelp.R
+
+
+class PengaturanFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate layout untuk fragment ini
+        return inflater.inflate(R.layout.fragment_pengaturan, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+
+        val profileCard: CardView = view.findViewById(R.id.card_profile)
+        profileCard.setOnClickListener {
+            // Pindah ke halaman detail profil
+            Toast.makeText(requireContext(), "Profile clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        val profileSetting: LinearLayout = view.findViewById(R.id.section_settings)
+        profileSetting.setOnClickListener {
+            // Pindah ke pengaturan
+            Toast.makeText(requireContext(), "Settings clicked!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
