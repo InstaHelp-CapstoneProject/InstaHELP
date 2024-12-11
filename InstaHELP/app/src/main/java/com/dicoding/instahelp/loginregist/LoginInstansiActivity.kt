8efac3760b3e68919d78a495d3e2dec01756fdee
Gatot <gatottriantono2003@gmail.com>
2024-12-12 06:23:28 +0700

    restore login

--- InstaHELP/app/src/main/java/com/dicoding/instahelp/loginregist/LoginInstansiActivity.kt
+++ InstaHELP/app/src/main/java/com/dicoding/instahelp/loginregist/LoginInstansiActivity.kt
@@ -2,17 +2,24 @@ package com.dicoding.instahelp.loginregist
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.CalendarContract.Instances
 import android.text.InputType
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
-import android.widget.Button
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.instahelp.API.ApiClient
+import com.dicoding.instahelp.API.ApiService
+import com.dicoding.instahelp.API.LoginInstitutionRequest
+import com.dicoding.instahelp.API.LoginResponse
 import com.dicoding.instahelp.R
 import com.dicoding.instahelp.databinding.ActivityLoginInstansiBinding
-import com.dicoding.instahelp.databinding.ActivityLoginMasyarakatBinding
+import com.dicoding.instahelp.resident.ResidentMainActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginInstansiActivity : AppCompatActivity() {
 
@@ -29,45 +36,79 @@ class LoginInstansiActivity : AppCompatActivity() {
             togglePasswordVisibility()
         }
 
-        val signupInstance = findViewById<TextView>(R.id.btn_signup_instance)
+        val loginInstance: TextView= findViewById(R.id.btn_next)
+        loginInstance.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Semua field harus diisi", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val loginRequest = LoginInstitutionRequest(
+                email = email,
+                password = password
+            )
+
+            // Mengirim permintaan login
+            ApiClient.apiService.loginInstitution(loginRequest).enqueue(object : Callback<LoginResponse> {
+                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                    if (response.isSuccessful) {
+                        val loginResponse = response.body()
+                        Log.d("LoginInstansiActivity", "Login successful: ${loginResponse?.message}")
+                        // Simpan token ke SharedPreferences
+                        val token = loginResponse?.message
+                        Log.d("LoginInstansiActivity", "Token received: $token")
+
+                        token?.let {
+                            val sharedPreferences = getSharedPreferences("user_preferences", MODE_PRIVATE)
+                            val editor = sharedPreferences.edit()
+                            editor.putString("bearer_token", it)
+                            editor.apply()
+                        }
+                        Toast.makeText(this@LoginInstansiActivity, "Login berhasil!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this@LoginInstansiActivity, ResidentMainActivity::class.java))
+                        finish()
+                    } else {
+                        val error = response.errorBody()?.string()
+                        Log.e("LoginInstansiActivity", "Error: $error")
+                        Toast.makeText(this@LoginInstansiActivity, "Login gagal: $error", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
+                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                    Log.e("LoginInstansiActivity", "Failure: ${t.message}")
+                    Toast.makeText(this@LoginInstansiActivity, "Terjadi kesalahan. Silakan coba lagi.", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
+        // Tombol untuk pindah ke halaman SignUp
+        val signupInstance = binding.btnSignupInstance
         signupInstance.setOnClickListener {
-            // Navigasi ke SignUpResidentActivity
             val intent = Intent(this, SignUpInstansiActivity::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
-        val loginInstance: TextView = findViewById(R.id.btn_next)
-        loginInstance.setOnClickListener {
-            val intent = Intent(this, SignUpInstansiActivity::class.java)
-            startActivity(intent) // Memulai aktivitas SignUpResidentActivity
-            overridePendingTransition(R.anim.activity_zoom_in, R.anim.activity_zoom_out)
-        }
-        val forgotPass: TextView = findViewById(R.id.tv_forgot_password_instance)
+
+        // Tombol untuk forgot password
+        val forgotPass: TextView = binding.tvForgotPasswordInstance
         forgotPass.setOnClickListener {
             val intent = Intent(this, ForgetPasswordActivity::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
-        loginInstance.text = getString(R.string.masuk)
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        val intent = Intent(this, SelectRoleActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivity(intent)
-        overridePendingTransition(R.anim.activity_zoom_in, R.anim.activity_zoom_out)
-        finish()
-    }
-
+    // Fungsi untuk menampilkan atau menyembunyikan password
     private fun togglePasswordVisibility() {
         if (isPasswordVisible) {
-            // Hide password
             binding.etPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
             binding.etPassword.transformationMethod = PasswordTransformationMethod.getInstance()
             binding.eyeIcon.setImageResource(R.drawable.eye_hid)
         } else {
-            // Show password
             binding.etPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
             binding.etPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
             binding.eyeIcon.setImageResource(R.drawable.eye_reveal)
@@ -76,5 +117,13 @@ class LoginInstansiActivity : AppCompatActivity() {
         binding.etPassword.setSelection(binding.etPassword.text.length)
     }
 
-
+    // Menangani tombol back untuk kembali ke aktivitas sebelumnya
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, SelectRoleActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        overridePendingTransition(R.anim.activity_zoom_in, R.anim.activity_zoom_out)
+        finish()
+    }
 }
--- InstaHELP/app/src/main/java/com/dicoding/instahelp/resident/ResidentMainActivity.kt
+++ InstaHELP/app/src/main/java/com/dicoding/instahelp/resident/ResidentMainActivity.kt
@@ -16,6 +16,7 @@ class ResidentMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_resident_main)
 
         // Default fragment
