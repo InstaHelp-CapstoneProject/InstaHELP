45bfea2243817fdb5366e492aaaf4c3ed64f9278
Xizene <arifkurniawan2004@gmail.com>
2024-12-11 20:35:17 +0700

    splashsing

--- InstaHELP/app/src/main/java/com/dicoding/instahelp/splashscreen/SplashActivity.kt
+++ InstaHELP/app/src/main/java/com/dicoding/instahelp/splashscreen/SplashActivity.kt
@@ -4,25 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.dicoding.instahelp.R
 import com.dicoding.instahelp.loginregist.GetStartedActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            goToGetStartedActivity()
+            goToGetStartActivity()
         }, 3000L)
     }
 
-    private fun goToGetStartedActivity() {
+    private fun goToGetStartActivity() {
         Intent(this, GetStartedActivity::class.java).also {
             startActivity(it)
             finish()
